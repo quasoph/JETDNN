439604da5b8956352aca7225750767b454bd81da
quasoph <S.Frankel2@newcastle.ac.uk>
2023-08-25 12:16:19 +0100

    Added author credits

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name = "JETDNN",
     version = "0.1",
-    author = "Sophie Frankel",
+    author = "S. Frankel, J. Simpson and E. Solano.",
     author_email = "s.frankel2@newcastle.ac.uk",
     url = "https://github.com/quasoph/JETDNN",
     description = "JETDNN: a Python package using Deep Neural Networks (DNNs) to find H-mode plasma pedestal heights from multiple engineering parameters.",
