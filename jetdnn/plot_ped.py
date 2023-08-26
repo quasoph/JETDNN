d27f37c2c346acd03d4e06a46e903315185e09f8
quasoph <S.Frankel2@newcastle.ac.uk>
2023-08-26 12:09:28 +0100

    developed plotting/visualising scripts

--- jetdnn/plot_ped.py
+++ jetdnn/plot_ped.py
@@ -10,3 +10,20 @@ from tensorflow.keras import layers
 """
 Function for plotting pedestals. Intended for use with predict_ped.predict_single function, to plot predicted pedestals using NPM.
 """
+
+def npm_plot(nepeds,delta_nes):
+
+    """
+    Plots a Neutral Penetration Model pedestal prediction. Could also be used with real data rather than predictions.
+    Only suitable for density pedestals.
+    """
+
+    C = 2 # set this equal to constant
+    x = np.linspace(0,5,100) # set tp cross section width
+    for n in range(0,10):
+        npm_vals = []
+        for i in range(x): # for each x value calculate a bunch of points
+            npm = nepeds[n] * np.tanh(C - (x/delta_nes[n]))
+            npm_vals.append(npm)
+        plt.plot(x,npm_vals)
+    return
--- jetdnn/visualise_model.py
+++ jetdnn/visualise_model.py
@@ -1,3 +1,12 @@
-"""
-Function for displaying a visualisation of the DNN model used.
-"""
+import tensorflow
+from tensorflow import keras
+import predict_ped
+from keras.utils.vis_utils import plot_model
+
+def graph_and_summary(model):
+    """
+    Function for displaying a visualisation of the DNN model used.
+    """
+    print(model.summary())
+    plot_model(model, to_file='dnn_model_plot.png', show_shapes=True, show_layer_names=True)
+    return
