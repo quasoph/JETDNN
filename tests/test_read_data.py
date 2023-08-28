55811c927d5681e4cd1a170fcd6542f86837d3a6
quasoph <S.Frankel2@newcastle.ac.uk>
2023-08-28 10:17:30 +0100

    edited docstring

--- tests/test_read_data.py
+++ tests/test_read_data.py
@@ -6,7 +6,7 @@ import pytest
 import sys
 
 def test_read_data():
-    """Tests basic functionality of single_ped function to read a .csv or .dat file."""
+    """Tests basic functionality of read_data function to read a .csv or .dat file."""
 
     testdata = [[10,55],[67,3],[97,35],[34,68],[1,12]]
     testdf = pd.DataFrame(testdata,columns=["One","Two","Three","Four","Five"])
