cc497292f832ab1c438254d8f41e25ec4a8ce214
quasoph <S.Frankel2@newcastle.ac.uk>
2023-08-31 12:05:33 +0100

    changes

--- tests/test_read_data.py
+++ tests/test_read_data.py
@@ -6,6 +6,8 @@ import pytest
 import sys
 from pathlib import Path
 
+import pkg_resources
+
 main_folder = Path(__file__).parent.parent
 sys.path.insert(0, str(main_folder))
 sys.path.insert(0, str(main_folder / 'jetdnn'))
@@ -13,11 +15,13 @@ sys.path.insert(0, str(main_folder / 'tests'))
 os.chdir(main_folder / 'jetdnn')
 
 import jetdnn
+g = pkg_resources.get_distribution("jetdnn").version
+print(g)
 
 def test_read_data():
     """Tests basic functionality of read_data function to read a .csv or .dat file."""
 
-    testdata = [[10,55],[67,3],[97,35],[34,68],[1,12]]
+    testdata = [[10,55,34,65,7],[67,3,45,6,8],[97,35,23,4,90]]
     testdf = pd.DataFrame(testdata,columns=["One","Two","Three","Four","Five"])
     testcsv = testdf.to_csv("filename.csv",index=False,encoding="utf-8")
 
