b63f5e270b1a8d8b24695a370979ee15b1111d49
quasoph <S.Frankel2@newcastle.ac.uk>
2023-08-31 11:24:12 +0100

    made tests discoverable

--- tests/test_read_data.py
+++ tests/test_read_data.py
@@ -4,6 +4,14 @@ import matplotlib.pyplot as plt
 import os
 import pytest
 import sys
+from pathlib import Path
+
+main_folder = Path(__file__).parent.parent
+sys.path.insert(0, str(main_folder))
+sys.path.insert(0, str(main_folder / 'jetdnn'))
+sys.path.insert(0, str(main_folder / 'tests'))
+os.chdir(main_folder / 'jetdnn')
+
 import jetdnn
 
 def test_read_data():
