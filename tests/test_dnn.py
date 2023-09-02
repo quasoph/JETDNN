c2f6d21d24511dca4c41d3181ad026e848fbef4f
Sophie Frankel <sophie.frankel814@gmail.com>
2023-09-02 20:32:54 +0100

    Wrote DNN testing functions

--- tests/test_dnn.py
+++ tests/test_dnn.py
@@ -16,5 +16,32 @@ os.chdir(main_folder / 'jetdnn')
 
 import jetdnn
 
-def test_dnn():
-    return
+# DNN TESTING FUNCTIONS
+"""
+The error provided is the error found for temperature data in the dataset used for "On neutral ground: can neutrals influence plasma pedestal formation?" by S. Frankel, J. Simpson and E. Solano
+It is suggested to use this dataset or temperature pedestal data for testing with the below functions.
+"""
+
+def test_build_and_test_single():
+    # check that predictions match actual data
+    test_data = pd.read_csv("filename.csv")
+    input_cols = ["B-field","I_p","triangularity"]
+    real_output_col = ["ped_height"]
+
+    output_expected = test_data[real_output_col]
+    output = jetdnn.predict.build_and_test_single(test_data,input_cols,real_output_col)[1] # returns flat_ped, or the predictions
+
+    assert output == pytest.approx(output_expected,abs(0.011)) # 0.011 keV based on temperature pedestal findings from summer placement
+
+def test_predict_single():
+    # check that predictions match actual data
+    test_data = pd.read_csv("filename.csv")
+    input_cols = ["B-field","I_p","triangularity"]
+    real_output_col = ["ped_height"]
+
+    model = jetdnn.predict.build_and_test_single(test_data,input_cols,real_output_col)[0]
+
+    output_expected = test_data[real_output_col]
+    output = jetdnn.predict.predict_single(model,test_data,input_cols)[1] # returns flat_ped, or the predictions
+
+    assert output == pytest.approx(output_expected,abs(0.011)) # 0.011 keV based on temperature pedestal findings from summer placement
