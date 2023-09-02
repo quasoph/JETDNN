26eb3d6ec49b73978de8462c1ecba0d0e670dd4b
Sophie Frankel <sophie.frankel814@gmail.com>
2023-09-02 20:35:15 +0100

    added info to jetdnn version output

--- jetdnn/visualise.py
+++ jetdnn/visualise.py
@@ -102,29 +102,4 @@ def display_model(model):
     plot_model(model, to_file='dnn_model_plot.png', show_shapes=True, show_layer_names=True)
     return
 
-# VISUALISE PREDICTIONS
-
-def density_npm_plot(nepeds,delta_nes):
-
-    """
-    density_npm_plot
-
-    Plot up to 10 predicted density pedestals using the Neutral Penetration Model (NPM).
-
-    Args:
-        nepeds (array): list of predicted density pedestal heights from build_and_test_single.
-        delta_nes (array): list of characteristic neutral penetration lengths, from H-mode data.
-
-    Returns:
-        None
-    """
-
-    C = 2 # set this equal to constant
-    x = np.linspace(0,5,100) # set tp cross section width
-    for n in range(0,10):
-        npm_vals = []
-        for i in range(x): # for each x value calculate a bunch of points
-            npm = nepeds[n] * np.tanh(C - (x/delta_nes[n]))
-            npm_vals.append(npm)
-        plt.plot(x,npm_vals)
-    return
+# density npm plot
