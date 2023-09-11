450fd0138d5a5977502e95cf6d8a097794a22552
Sophie Frankel <99756733+quasoph@users.noreply.github.com>
2023-09-11 16:14:25 +0100

    Update visualise.py
    - Changed equation builder format from string to array (correcting final equation built)
    
    - Changed relu activation to np.maximum function for better efficiency & accuracy
    
    - Used sympy to simplify final equation, removed simplifying section of code

--- jetdnn/visualise.py
+++ jetdnn/visualise.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import re
+from sympy import *
 
 # get equation
 
@@ -29,12 +30,11 @@ def get_equation(model,filename,input_cols,output_col):
 
     def activation_relu(input):
         
-        for num in input.tolist():
-            if num <= 0:
-                return 0
-        return input
+        output = np.maximum(0,input)
+
+        return output
         
-    equation = ""
+    equation = []
 
     data = pd.read_csv(filename,sep="\s{3,}|\s{3,}|\t+|\s{3,}\t+|\t+\s{3,}",skipinitialspace=True)
 
@@ -57,47 +57,53 @@ def get_equation(model,filename,input_cols,output_col):
                 for j in range(0,layer_nodes[n]): # for each node (e.g. for each weight in w0 e.g. w01, w02, w03 etc)
                     node = 0
                     node_eqn = ""
+
                     for i in range(0,len(input_cols)): # for each input, add weights and inputs to node
+
+                        if n == 1:
+                            x_eqn = "x" + str(i)
                         
                         if isinstance(x,pd.DataFrame):
                             col = np.array(x.iloc[:,i]) # gets ith column, converts to numpy array
                         else:
                             col = x[i]
                         
-                        node += (col * float(w[i,j])) # gives the jth value in i: ERROR: for some reason w[i,j] has shape 128, should have 3 lists of 128 vals each. Suggest printing weights array for debugging (important). Check get_weights MS Edge folder
-                        node_eqn += "x" + str(i) + "*" + str(w[i,j]) + " + "
-                    
-                    if activation_relu(node + b[j]) is not 0:
-                        node += b[j]
-                        node_eqn += str(b[j])
+                        node += (col * float(w[i,j])) # gives the jth value in i
+                        
+                        if x_eqn != "x" + str(i):
+                        
+                            for y in equation:
+                                x_eqn = y
+                                node_eqn += "(" + str(x_eqn) + ")" + "*" + str(w[i,j]) + " + " # multiply each node eqn by weight and sum
 
-                    nodes.append(activation_relu(node)) # add node to node list
-                    equation += node_eqn # adds every node to the main equation
+                        else:
+
+                            node_eqn += str(x_eqn) + "*" + str(w[i,j]) + " + "
+                    
+                    node += b[j] # node now contains weights and bias
+                    node_eqn += str(b[j])
+                    
+                    if activation_relu(node) is not 0: # if +ive node value
+
+                        nodes.append(activation_relu(node)) # add node to node list for that layer
+                        equation.append(node_eqn) # add node equation to list for that layer
+
 
                 x = nodes # node values become new inputs
+                x_eqn = equation # list of node equations for that layer
+                
     
 
     # now out of all loops
     
-    added_weights = np.zeros(len(input_cols))
-    short_eqn = ""
+    final_eqn = equation[-1] # this should be the final equation
 
-    for i in range(0,len(input_cols)):
-        coeff_res = []
-        substring = "x" + str(i)
-        res = [n for n in range(0,len(equation)) if equation.startswith(substring,n)] # finds positions of xi
-        for q in res:
-            coeff_res.append(q+3) # gets positions of x-coefficients
-        for w in coeff_res:
-            added_weights[i] += float(equation[w:w+4]) # sums coefficients of xi
+    smpl = simplify(final_eqn)
 
-        short_eqn += str(np.round(added_weights[i],3)) + " [" + str(input_cols[i]) + "] " + " + "
+    print(final_eqn)
+    print(smpl)
 
-    print(str(output_col) + " = " + short_eqn)
-
-    print(equation)
-
-    return short_eqn, equation, np.array(x)
+    return smpl, final_eqn, x
 
 
 # VISUALISE MODELS
